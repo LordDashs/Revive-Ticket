10afd8b4779ea17a3df8175d9c94be708165ce52
LordDashs <148006327+LordDashs@users.noreply.github.com>
2026-05-12 18:53:43 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -22,10 +22,10 @@ export const botConfig = {
     // 3 = Watching
     // 4 = Custom
     // 5 = Competing
-    activities: [
+    activities: 3
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "S'occupe des ticket 🤖",
         // Activity type number (0 = Playing).
         type: 0, 
       },
